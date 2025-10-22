47f19760684f8bbb9ef6fef270023ce2d878bc1b
todanh2706 <thdanh23@clc.fitus.edu.vn>
2025-10-22 13:21:23 +0700

    Added example.md to test remark-gfm plugin

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,39 +1,21 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
 
 function App() {
-  // const [count, setCount] = useState(0)
-  const markdown = '# Hi, *I\'m Markdown rendering tool*!';
+  const [markdownContent, setMarkdownContent] = useState('');
+
+  useEffect(() => {
+    fetch('/example.md')
+      .then((res) => res.text())
+      .then((text) => setMarkdownContent(text))
+  }, [])
+
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="text-green-500">
-        Hello World!
-      </p> */}
-
-      <Markdown>{markdown}</Markdown>
+      <Markdown remarkPlugins={[remarkGfm]}>{markdownContent}</Markdown>
     </>
   )
 }
